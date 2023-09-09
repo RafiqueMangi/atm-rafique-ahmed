79467576bd939cb455e88c4e4bf9a11085356ada
Rafique Ahmed <142241546+rafiquemangi@users.noreply.github.com>
2023-09-09 02:44:51 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,72 @@
+import inquirer from "inquirer";
+
+interface ansType {
+    userId: string
+    userPin: number
+    accountType: string
+    transationType:string
+    amount: number
+}
+const answers: ansType = await inquirer.prompt([
+    {
+type:"input",
+name:"userId",
+message:"kindly Enter Your Id"
+},
+{
+    type:"number",
+    name:"userPin",
+    message:"kindly Enter Your Pin"
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["current", "saving", ],
+        message: "select your account type",
+    
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["fast Cash", "withdraw", ],
+        message: "select your transaction",
+        when(answers) {
+            return answers.accountType
+      },
+
+    },
+    
+    {
+        type: "list",
+        name: "amount",
+        choices: [500,5000,10000,20000],
+        message: "select your amount",
+        when(answers) {
+            return answers.transactionType == "fast Cash"
+      },
+
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount",
+        when(answers) {
+            return answers.transactionType == "withdraw"
+      },
+
+    },
+    
+
+])
+if (answers.userId && answers.userPin){
+    const balance = Math.floor (Math.random()*10000000);
+    console.log(balance)
+    const EnteredAmount = answers.amount;
+    if (balance >= EnteredAmount){
+const remianing = balance - EnteredAmount;
+console.log( "your remaining balance is", remianing)
+    } else {
+        console.log("Insuficient Balance  ")
+    }
+
+}
